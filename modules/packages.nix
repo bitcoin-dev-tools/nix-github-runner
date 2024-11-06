7784ff3b522776d751b283d8d6789558ab69dfc9
willcl-ark <will@256k1.dev>
2024-11-06 20:31:31 +0000

    add hyper wrapper

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,4 +1,20 @@
 { config, lib, pkgs, ... }:
+let
+  hyper-wrapper = pkgs.rustPlatform.buildRustPackage rec {
+    pname = "hyper-wrapper";
+    version = "0.1.0";
+    src = pkgs.fetchCrate {
+      inherit pname version;
+      sha256 = "sha256-11HJdxUshs+qfAqw4uqmY7z+XIGkdeUD9O4zl4fvDdE=";
+    };
+    cargoHash = "sha256-ffChU1z8VC2y7l6Pb/eX2XXdFDChMwnroSfsHIVChds=";
+    meta = with pkgs.lib; {
+      description = "Hyperfine wrapper";
+      homepage = "https://github.com/bitcoin-dev-tools/hyper-wrapper";
+      license = licenses.mit;
+    };
+  };
+in
 {
   environment.systemPackages = with pkgs; [
     # Development tools
@@ -12,7 +28,6 @@
     gnutar
     podman
     python3
-
     # Shell utilities
     bat
     curl
@@ -23,10 +38,15 @@
     neovim
     ripgrep
     tmux
-
     # System tools
     ccache
     magic-wormhole
     time
+    # Benchmarking
+    hyperfine
+    hyper-wrapper
+    perf-tools
+    flamegraph
+    linuxKernel.packages.linux_6_6.perf
   ];
 }
