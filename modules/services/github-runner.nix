9cc70982a0e690f49ef0d55d130dc7e392c43c7d
willcl-ark <will@256k1.dev>
2025-01-09 08:57:48 +0000

    runner: restart github-runner even on failure
    This protects against github API errors causing a failure and requiring
    manual restart.
    
    RestartForceExitStatus = [ 0 1 2 ]: Forces a restart for all common exit codes
    
    0: Success
    1: General failure
    2: Retryable error (built in to default service definition)
    
    SuccessExitStatus = [ 0 1 2 ]: Marks these codes as "successful" exits to prevent systemd from treating them as failures.
    
    With this the service will restart regardless of how it exits, while still maintaining some rate limiting protections.

--- modules/services/github-runner.nix
+++ modules/services/github-runner.nix
@@ -1,22 +1,31 @@
-{ config, pkgs, ... }:
-let runner_token = builtins.getEnv "RUNNER_TOKEN";
+{ config, pkgs, lib, ... }:
+let
+  runner_token = builtins.getEnv "RUNNER_TOKEN";
 in {
   virtualisation.docker.enable = true;
 
-  users.groups.github-runner = { };
+  users.groups = {
+    github-runner = {};
+    perf = {};
+  };
+
   users.users.github-runner = {
     isNormalUser = true;
     group = "github-runner";
     extraGroups = [ "docker" "perf" ];
   };
-  users.groups.perf = { };
 
   systemd.tmpfiles.rules = [
     "d /data/runner_workspace 0755 github-runner github-runner -"
     "d /data/ccache 0755 github-runner github-runner -"
   ];
 
-  environment.etc = { gh_token = { text = runner_token; }; };
+  environment.etc.gh_token.text = runner_token;
+
+  systemd.services.github-runner-ax52 = {
+    startLimitIntervalSec = 300;
+    startLimitBurst = 3;
+  };
 
   services.github-runners.ax52 = {
     enable = true;
@@ -26,12 +35,25 @@ in {
     ephemeral = true;
     workDir = "/data/runner_workspace";
     replace = true;
-    extraPackages = with pkgs; [ config.virtualisation.docker.package ccache ];
+    extraPackages = with pkgs; [
+      config.virtualisation.docker.package
+      ccache
+    ];
     serviceOverrides = {
       ProtectHome = false;
-      ReadWritePaths = [ "/data/ccache" "/data/runner_workspace" ];
+      ReadWritePaths = [
+        "/data/ccache"
+        "/data/runner_workspace"
+      ];
       AmbientCapabilities = [ "CAP_SYS_RAWIO" ];
       CapabilityBoundingSet = [ "CAP_SYS_RAWIO" ];
+      # Rate limit unsucessful restarts
+      StartLimitBurst = 3;
+      StartLimitIntervalSec = 300;
+      # Restart on any exit code
+      RestartForceExitStatus = [ 0 1 2 ];
+      # Override the default succeed-exit-codes to ensure restarts happen
+      SuccessExitStatus = [ 0 1 2 ];
     };
   };
 }
