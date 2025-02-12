ecaa0a10d82697b5b96bf8e5d255f6aba70ed369
willcl-ark <will@256k1.dev>
2025-02-12 10:05:29 +0000

    stabilise benchmarking config

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -6,7 +6,20 @@
     };
 
     kernelModules = [ "msr" "cpuid" "x86_pkg_temp_thermal" ];
-    kernelParams = [ "processor.max_cstate=1" "idle=poll" "tsc=reliable" ];
+    # Add pyperf system advice:
+    # =======
+    # Perf event: Set max sample rate to 1
+    # ASLR: Enable full randomization: write 2 into /proc/sys/kernel/randomize_va_space
+    # Linux scheduler: Use isolcpus=<cpu list> kernel parameter to isolate CPUs
+    # Linux scheduler: Use rcu_nocbs=<cpu list> kernel parameter (with isolcpus) to not schedule RCU on isolated CPUs
+    kernelParams = [
+      "processor.max_cstate=1"
+      "idle=poll"
+      "tsc=reliable"
+      "isolcpus=2-15" # Isolate CPUs 2-15 from the general scheduler
+      "nohz_full=2-15" # Enable full dynticks system for isolated CPUs
+      "rcu_nocbs=2-15" # Don't run RCU callbacks on isolated CPUs
+    ];
     kernelPackages = pkgs.linuxPackages_6_6;
 
     kernel.sysctl = {
@@ -17,7 +30,7 @@
       "kernel.perf_cpu_time_max_percent" = 75;
       "kernel.perf_event_max_sample_rate" = 100000;
       "kernel.perf_event_paranoid" = -1;
-      "kernel.randomize_va_space" = 0;
+      "kernel.randomize_va_space" = 2; # Enable full randomization
       "kernel.sched_autogroup_enabled" = 0;
       "kernel.sched_migration_cost_ns" = 5000000;
       "kernel.sched_min_granularity_ns" = 10000000;
